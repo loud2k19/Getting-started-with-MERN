57fb9630ae3717f1b3fa9d5505ffedaed4620be2
loud2k19 <himeshcody@pec.edu>
2020-05-24 11:02:16 +0530

    initial_try

--- server.js
+++ server.js
@@ -2,5 +2,4 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => res.send("API is running"));
 app.listen(PORT, () => console.log(`Server has startet ${PORT}`));
