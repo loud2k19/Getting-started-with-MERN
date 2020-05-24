5d634097effa64ba1c64daa2ea80af1d6b664a2b
loud2k19 <himeshcody@pec.edu>
2020-05-24 11:43:54 +0530

    routes

--- server.js
+++ server.js
@@ -1,5 +1,15 @@
 const express = require("express");
+const connectDB = require("./config/db");
 const app = express();
-const PORT = process.env.PORT || 5000;
 
+connectDB();
+app.get("/", (req, res) => res.send("API is running"));
+
+//define routes
+app.use("/api/users", require("./routes/api/user"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/post", require("./routes/api/post"));
+
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server has startet ${PORT}`));
