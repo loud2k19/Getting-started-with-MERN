682d47ee19ef0aa3b826f31f3391722877a2fa9d
loud2k19 <himeshcody@pec.edu>
2020-05-26 08:33:06 +0530

    ready

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const connectDB = require("./config/db");
+const path = require("path");
 const app = express();
 
 //connect db
 connectDB();
-app.get("/", (req, res) => res.send("API is running"));
 
 //init middleware
 app.use(express.json({ extend: false }));
@@ -14,6 +14,14 @@ app.use("/api/user", require("./routes/api/user"));
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/post", require("./routes/api/post"));
+
+//for static assests for productin
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`Server has startet ${PORT}`));
